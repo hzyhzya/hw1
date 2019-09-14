6493925ce68357af930798b67e4164a297b81e7a
hzyhzya <55303885+hzyhzya@users.noreply.github.com>
2019-09-13 18:52:23 -0700

    Update Bubble.js

--- Bubble.js
+++ Bubble.js
@@ -3,11 +3,11 @@ var bubbles = [];
 function setup() {
   createCanvas(800, 600);
 
-  for (var i = 0; i < 10; i++) {
+  for (var i = 0; i < 30; i++) {
     var bubble = {
       x: random(width),
       y: random(height),
-      radius: random(50, 100)
+      radius: random(30, 60)
     };
     bubbles.push(bubble);
   }
@@ -23,13 +23,13 @@ function draw() {
       if (mouseIsPressed) {
         bubbles.splice(i, 1); // remove this bubble!
       }
-      fill(255, 200, 200, 200);
+      fill(05, 80, 890, 100);
     } else {
-      fill(255, 220, 200, 200);
+      fill(25, 020, 50, 80);
     }
 
-    ellipse(bubble.x, bubble.y, bubble.radius * 2);
-    bubble.x += random(-1, 1);
-    bubble.y += random(-1, 1);
+    ellipse(bubble.x, bubble.y, bubble.radius * 3);
+    bubble.x += random(-2, 2);
+    bubble.y += random(-2, 2);
   }
 }
